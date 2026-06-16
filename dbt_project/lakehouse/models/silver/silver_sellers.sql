95495b5b82ed4895c2b40009330f08cb7cb984e0
Felipe Caron <felipe.caronap@outlook.com>
2026-06-16 17:24:39 -0300

    add: modelo silver_sellers com validacao de UF e preservacao do original

---
+++ dbt_project/lakehouse/models/silver/silver_sellers.sql
@@ -0,0 +1,37 @@
+{{ config(
+    materialized='external',
+    location='../../data/silver/silver_sellers.parquet'
+) }}
+
+WITH bronze AS (
+    SELECT *
+    FROM read_parquet('../../data/bronze/sellers.parquet')
+),
+
+tratado AS (
+    SELECT
+        seller_id,
+        seller_name,
+        seller_city,
+        seller_state AS seller_state_original,
+        CASE
+            WHEN seller_state IN (
+                'AC','AL','AP','AM','BA','CE','DF','ES','GO',
+                'MA','MT','MS','MG','PA','PB','PR','PE','PI',
+                'RJ','RN','RS','RO','RR','SC','SP','SE','TO'
+            ) THEN seller_state
+            ELSE 'Não identificado'
+        END AS seller_state,
+        CASE
+            WHEN seller_state IN (
+                'AC','AL','AP','AM','BA','CE','DF','ES','GO',
+                'MA','MT','MS','MG','PA','PB','PR','PE','PI',
+                'RJ','RN','RS','RO','RR','SC','SP','SE','TO'
+            ) THEN TRUE
+            ELSE FALSE
+        END AS estado_valido
+    FROM bronze
+)
+
+SELECT *
+FROM tratado
