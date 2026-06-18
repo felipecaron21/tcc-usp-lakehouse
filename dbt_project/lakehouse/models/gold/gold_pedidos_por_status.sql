089db411b50d9809c2b629f854339d947b737379
Felipe Caron <felipe.caronap@outlook.com>
2026-06-18 00:45:05 -0300

    add: modelo gold_pedidos_por_status

---
+++ dbt_project/lakehouse/models/gold/gold_pedidos_por_status.sql
@@ -0,0 +1,16 @@
+{{ config(
+    materialized='external',
+    location='../../data/gold/gold_pedidos_por_status.parquet'
+) }}
+
+WITH silver_orders AS (
+    SELECT *
+    FROM {{ ref('silver_orders') }}
+)
+
+SELECT
+    order_status AS status,
+    COUNT(*) AS total_pedidos
+FROM silver_orders
+GROUP BY order_status
+ORDER BY total_pedidos DESC
