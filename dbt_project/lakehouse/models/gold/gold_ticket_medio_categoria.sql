a478dbc5cd2c92a06b977b17cdad7f4a386d2437
Felipe Caron <felipe.caronap@outlook.com>
2026-07-12 18:25:23 -0300

    add: modelo gold_ticket_medio_categoria

---
+++ dbt_project/lakehouse/models/gold/gold_ticket_medio_categoria.sql
@@ -0,0 +1,26 @@
+{{ config(
+    materialized='external',
+    location='../../data/gold/gold_ticket_medio_categoria.parquet'
+) }}
+
+WITH order_items AS (
+    SELECT *
+    FROM {{ ref('silver_order_items') }}
+    WHERE pedido_orfao = FALSE
+    AND preco_invalido = FALSE
+),
+
+products AS (
+    SELECT *
+    FROM {{ ref('silver_products') }}
+)
+
+SELECT
+    p.product_category AS categoria,
+    ROUND(AVG(oi.price), 2) AS ticket_medio,
+    COUNT(*) AS total_itens
+FROM order_items oi
+LEFT JOIN products p
+    ON oi.product_id = p.product_id
+GROUP BY p.product_category
+ORDER BY ticket_medio DESC
