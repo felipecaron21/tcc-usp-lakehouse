26c20633a288fb4d1aa09bf9c089810834de19ae
Felipe Caron <felipe.caronap@outlook.com>
2026-07-12 18:31:28 -0300

    add: modelo gold_top_sellers

---
+++ dbt_project/lakehouse/models/gold/gold_top_sellers.sql
@@ -0,0 +1,29 @@
+{{ config(
+    materialized='external',
+    location='../../data/gold/gold_top_sellers.parquet'
+) }}
+
+WITH order_items AS (
+    SELECT *
+    FROM {{ ref('silver_order_items') }}
+    WHERE pedido_orfao = FALSE
+    AND preco_invalido = FALSE
+),
+
+sellers AS (
+    SELECT *
+    FROM {{ ref('silver_sellers') }}
+    WHERE estado_valido = TRUE
+)
+
+SELECT
+    s.seller_name,
+    s.seller_state,
+    ROUND(SUM(oi.price), 2) AS volume_vendas,
+    COUNT(*) AS total_itens
+FROM order_items oi
+INNER JOIN sellers s
+    ON oi.seller_id = s.seller_id
+GROUP BY s.seller_name, s.seller_state
+ORDER BY volume_vendas DESC
+LIMIT 5
